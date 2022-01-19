eb99684fd59d02c0c7e6ef47a3d4beec12d1ade1
tomokay <moka7717@gmail.com>
2022-01-19 11:33:41 +0900

    refs #2, add TopPage

---
+++ src/components/Top.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "../App.css";
+
+const Top = () => {
+  return (
+    <>
+      <div className="App">
+        <header className="App-header">
+          温泉検索サイト
+          <button>ログイン</button>
+          <button>新規登録</button>
+        </header>
+      </div>
+    </>
+  );
+};
+
+export default Top;
