70e9f5561287db2a1b1971964d45575e1692b306
tomokay <moka7717@gmail.com>
2022-04-10 03:37:02 +0900

    バックエンドuriを環境変数に変更

--- src/apollo-client.ts
+++ src/apollo-client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:3002/graphql",
+  uri: process.env.REACT_APP_BACKEND_URL,
   cache: new InMemoryCache(),
 });
 
