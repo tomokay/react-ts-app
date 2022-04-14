6e4e20a2ea3b50d06519af87f25c8835ed8276ed
Furukawa Kazuaki <furukawakazuaki@FurukawanoiMac.local>
2022-04-14 23:15:41 +0900

    refs #25, 登録の一連フローを改修

--- src/pages/SpaEntryPage.tsx
+++ src/pages/SpaEntryPage.tsx
@@ -2,17 +2,20 @@ import React, { useEffect, useState } from "react";
 import Stepper from "@mui/material/Stepper";
 import Step from "@mui/material/Step";
 import StepLabel from "@mui/material/StepLabel";
-import { Button, Grid } from "@mui/material";
+import { Box, Button, CircularProgress, Grid, Typography } from "@mui/material";
 import { SpaInputInfomation } from "src/components/spa/SpaInputInformation";
 import { CREATE_SPA } from "src/graphql/createSpa";
 import { useMutation } from "@apollo/client";
-import { NavLink, useLocation } from "react-router-dom";
+import { Link, NavLink, useLocation } from "react-router-dom";
 import { fetchSpa } from "src/lib/fetchSpa";
 import { UPDATE_SPA } from "src/graphql/updateSpa";
 import { TitleHeader } from "src/components/commons/TitleHeader";
 import { makeStyles } from "@mui/styles";
 import { Spa } from "src/components/Types";
 import { SpaInputConfirm } from "src/components/spa/SpaInputConfirm";
+import { SuccessDialog } from "src/components/dialogs/SuccessDialog";
+import { AlertDialog } from "src/components/dialogs/AlertDialog";
+import Backdrop from "@mui/material/Backdrop";
 
 const useStyles = makeStyles({
   contents: {
@@ -35,56 +38,14 @@ const useStyles = makeStyles({
 
 const SpaEntryPage = () => {
   const [activeStep, setActiveStep] = useState<number>(0);
-  const [spa, setSpa] = useState<Spa>({
-    basic: {
-      spaName: "",
-      address: "",
-      phoneNumber: "",
-      businessHours: "",
-      regularHoliday: "",
-    },
-    price: {
-      adultPrice: 0,
-      childPrice: 0,
-      adultWeekendPrice: 0,
-      childWeekendPrice: 0,
-    },
-    amenity: {
-      hasFreeShampoo: false,
-      hasPaidShampoo: false,
-      hasTowel: false,
-      hasFreeHairdryer: false,
-      hasPaidHairdryer: false,
-      hasCreditCard: false,
-    },
-    spaFacility: {
-      hasOpenAirBath: false,
-      hasWaterBath: false,
-      hasSauna: false,
-      hasBubbleBath: false,
-      hasJetBathSpa: false,
-      hasShoulderHittingShower: false,
-      hasSleepingBath: false,
-      hasCypressBath: false,
-      hasBedrockBath: false,
-      hasElectricBath: false,
-      hasFamilyBath: false,
-      customSpa: "",
-    },
-    anotherFacility: {
-      hasRestaurant: false,
-      hasBreakPlace: false,
-      hasMassageMachine: false,
-      hasVendingMachine: false,
-      hasStore: false,
-      customFacility: "",
-    },
-    picture: "",
-    lat: "",
-    lng: "",
-  });
+  const [spa, setSpa] = useState<Spa>(initialSpa);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [isSuccesModalOpen, setIsSuccessModalOpen] = useState<boolean>(false);
+  const [isAlertModalOpen, setIsAlertModalOpen] = useState<boolean>(false);
 
-  const [createSpa] = useMutation(CREATE_SPA);
+  const [createSpa] = useMutation(CREATE_SPA, {
+    errorPolicy: "all",
+  });
   const [updateSpa] = useMutation(UPDATE_SPA);
 
   const location = useLocation() as any;
@@ -105,6 +66,7 @@ const SpaEntryPage = () => {
   const getSteps = () => {
     return ["入力", "確認", "完了"];
   };
+  const pageTitles = ["情報入力", "入力内容確認", "登録完了"];
 
   const getStepContent = (stepIndex: number) => {
     switch (stepIndex) {
@@ -121,8 +83,26 @@ const SpaEntryPage = () => {
       case 2:
         return (
           <>
-            <span>登録完了</span>
-            <NavLink to="/spa">温泉一覧</NavLink>
+            <Grid
+              container
+              justifyContent="center"
+              alignItems="center"
+              style={{ marginTop: "150px" }}
+            >
+              <Grid item>
+                <Typography variant="h5" gutterBottom component="div">
+                  登録完了
+                </Typography>
+                <Button
+                  color="inherit"
+                  variant="outlined"
+                  component={Link}
+                  to="/"
+                >
+                  温泉一覧に戻る
+                </Button>
+              </Grid>
+            </Grid>
           </>
         );
       default:
@@ -132,8 +112,39 @@ const SpaEntryPage = () => {
 
   const steps = getSteps();
 
-  const handleNext = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  const handleNext = async () => {
+    if (activeStep === 0) {
+      setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    }
+
+    if (activeStep === 1) {
+      try {
+        // 新規登録の処理
+
+        setLoading(true);
+        const result = await createSpa({
+          variables: {
+            input: spa,
+          },
+        });
+
+        if (!result.data) {
+          throw new Error(`${result.errors}`);
+        }
+
+        if (result.data) {
+          setIsSuccessModalOpen(true);
+          setActiveStep((prevActiveStep) => prevActiveStep + 1);
+        }
+      } catch (err) {
+        console.log("err: ", err);
+        setIsAlertModalOpen(true);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    return;
   };
   const handleBack = () => {
     setActiveStep((prevActiveStep) => prevActiveStep - 1);
@@ -141,10 +152,28 @@ const SpaEntryPage = () => {
 
   const classes = useStyles();
 
+  if (loading) {
+    return (
+      <Backdrop
+        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={loading}
+      >
+        <CircularProgress />
+      </Backdrop>
+    );
+  }
+
+  const handleSuccessModalClose = () => {
+    setIsSuccessModalOpen(false);
+  };
+  const handleAlertModalClose = () => {
+    setIsAlertModalOpen(false);
+  };
+
   return (
     <>
       <div className={classes.contents}>
-        <TitleHeader HeaderTitle="温泉情報入力" />
+        <TitleHeader HeaderTitle={pageTitles[activeStep]} />
         <Stepper
           activeStep={activeStep}
           alternativeLabel
@@ -159,29 +188,95 @@ const SpaEntryPage = () => {
         <Grid container direction="column" alignItems="center">
           <Grid item>{getStepContent(activeStep)}</Grid>
         </Grid>
-        <div>
-          <Button
-            variant="contained"
-            color="primary"
-            className={classes.button}
-            onClick={handleNext}
-          >
-            次へ
-          </Button>
-        </div>
-        <div>
-          <Button
-            hidden={activeStep === 0}
-            variant="outlined"
-            className={classes.button}
-            onClick={handleBack}
-          >
-            戻る
-          </Button>
-        </div>
+
+        {activeStep !== 2 ? (
+          <>
+            <div>
+              <Button
+                variant="contained"
+                color="primary"
+                className={classes.button}
+                onClick={async () => await handleNext()}
+              >
+                次へ
+              </Button>
+            </div>
+            <div>
+              <Button
+                hidden={activeStep === 0}
+                variant="outlined"
+                className={classes.button}
+                onClick={handleBack}
+              >
+                戻る
+              </Button>
+            </div>
+          </>
+        ) : (
+          <></>
+        )}
+        <SuccessDialog
+          isOpen={isSuccesModalOpen}
+          message="情報登録が完了しました。"
+          handleClose={handleSuccessModalClose}
+        />
+        <AlertDialog
+          isOpen={isAlertModalOpen}
+          message="情報登録に失敗しました。お手数ですが再度お試しください。"
+          handleClose={handleAlertModalClose}
+        />
       </div>
     </>
   );
 };
 
 export default SpaEntryPage;
+
+const initialSpa = {
+  basic: {
+    spaName: "",
+    address: "",
+    phoneNumber: "",
+    businessHours: "",
+    regularHoliday: "",
+  },
+  price: {
+    adultPrice: 0,
+    childPrice: 0,
+    adultWeekendPrice: 0,
+    childWeekendPrice: 0,
+  },
+  amenity: {
+    hasFreeShampoo: false,
+    hasPaidShampoo: false,
+    hasTowel: false,
+    hasFreeHairdryer: false,
+    hasPaidHairdryer: false,
+    hasCreditCard: false,
+  },
+  spaFacility: {
+    hasOpenAirBath: false,
+    hasWaterBath: false,
+    hasSauna: false,
+    hasBubbleBath: false,
+    hasJetBathSpa: false,
+    hasShoulderHittingShower: false,
+    hasSleepingBath: false,
+    hasCypressBath: false,
+    hasBedrockBath: false,
+    hasElectricBath: false,
+    hasFamilyBath: false,
+    customSpa: "",
+  },
+  anotherFacility: {
+    hasRestaurant: false,
+    hasBreakPlace: false,
+    hasMassageMachine: false,
+    hasVendingMachine: false,
+    hasStore: false,
+    customFacility: "",
+  },
+  picture: "",
+  lat: "",
+  lng: "",
+};
